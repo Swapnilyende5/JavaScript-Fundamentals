bfa8780b2ce6217b17b73684b45baa04d862a9b5
swapnilyende5 <swapnilyende5@gmail.com>
2025-11-12 23:31:38 +0530

    practice day4

--- callByValueCallByReference.js
+++ callByValueCallByReference.js
@@ -1,7 +1,48 @@
 // 1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣🔟
 
-// ------------ Call By Reference -------------
+// ------------ 1️⃣ Call By Reference -------------
 // works with Non-Primitive Data Types like (Objects and Arrays)
+
 const person = { name: "Swapnil" };
-// ✅ The actual object { name: "Swapnil" } is stored in heap
-// ✅ The variable person in stack stores only a reference (a memory address)
+
+// The actual object { name: "Swapnil" } is stored in heap
+// The variable "person" in stack stores only a reference (a memory address)
+
+// Stack memory:
+// person: → 0xA12B  🚀 (reference)
+
+// Heap memory:
+// 0xA12B --> { name: "Swapnil" }
+
+// So person doesn’t store the object —
+// It stores a pointer to where the object lives.
+
+const a = { value: 10 };
+const b = a; // SAME reference
+
+// a: → 0xB22C
+// b: → 0xB22C (same address)
+
+// Heap:
+// 0xB22C --> { value: 10 }
+
+const user = { name: "Swapnil" };
+const employee = user;
+employee.name = "Rahul";
+console.log(user, employee);
+// --------------------------------------------
+
+// ------------ 1️⃣ Call By Value -------------
+let c = 10;
+let d = c;
+
+// a stores actual value 10 in memory
+// b receives a new separate copy
+// Changing b doesn’t affect a
+
+// Stack Memory
+// a: 10
+// b: 20  ← independent copy
+c = 20;
+console.log(c, d);
+// --------------------------------------------------
--- practice-set-1.js
+++ practice-set-1.js
@@ -61,3 +61,101 @@ for (let [k, v] of newMap) {
   console.log(k);
   console.log(v);
 }
+// ------------ 5️⃣. IIFE (Immediately Invoked Function Expression) ---------------------
+// Avoid global variable pollution
+// Keeps variables private
+// Execute code immediately
+(function () {
+  const newMap = "Map";
+  console.log(newMap);
+})();
+
+// Arrow IIFE
+((name) => {
+  console.log(name);
+})("Swapnil");
+
+// ------------ 6️⃣. Higher Order Functions (HOF) ---------------------
+
+// 1. Takes another function as an argument,
+function greet(name) {
+  return `Hello ${name}`;
+}
+function userInput(callback) {
+  let username = "Swapnil";
+  return callback(username);
+}
+console.log(userInput(greet));
+
+// 2. Returns a function as its result.
+function multiplier(value) {
+  return function (number) {
+    return number * value;
+  };
+}
+const double = multiplier(2);
+console.log(double(5));
+
+// Examples: Built-in HOF's => map(), filter(), reduce(), forEach();
+
+// ------------ 7️⃣. Higher Order Functions (HOF) ---------------------
+
+// A callback function is a function passed as an argument to another function, which is then executed inside that function.
+// It’s called a “callback” because it’s called back later — not immediately.
+function greet1(name) {
+  console.log(`Hello, ${name}!`);
+}
+function processUserInput(callback) {
+  const userName = "Swapnil";
+  callback(userName);
+}
+processUserInput(greet1); // greet1 is a callback function
+
+// document.getElementById("btn").addEventListener("click", () => {
+//   console.log("Button clicked!");
+// });
+// here, ()=> {} ia callback func, calls only when button is clicked
+// we always calls
+
+// ------------ 8️⃣ Pure and Impure  ---------------------
+
+// 1. Pure function - is the function produces the same output for the same input
+function add(a, b) {
+  return a + b;
+}
+console.log(add(4, 5));
+
+function calculateTax(amount, taxRate) {
+  return amount * taxRate;
+}
+console.log(calculateTax(10000, 0.5));
+// in above both functions, output is totally depends on the input arguments and receivables parameters.
+
+// 2. Pure function is the function wherein we use the external values for operations and o/p can be changed.
+let total = 0;
+function addition(val) {
+  return (total += val);
+}
+console.log(addition(5));
+console.log(addition(5));
+
+let tax = 0.5; // if someone change the external tax then function can return diff o/p.
+function calcTax(amount) {
+  return amount * tax;
+}
+console.log(calcTax(10000));
+
+// to make the function Pure, we always have to use the params received. not the external val's
+function calculateTax(amount, taxRate) {
+  return amount * taxRate;
+}
+console.log(calculateTax(10000, 0.5));
+
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.time("Without Memoization");
+console.log(fibonacci(40)); // Takes several seconds
+console.timeEnd("Without Memoization");
